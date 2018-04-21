59f16e0e44b8873f58132d6a940bc794f68e7840
peymanvalikhanli <peymanvalikhanli2012@gmail.com>
2018-04-21 14:53:23 +0430

    edit user profile connect to server

--- screens/profile.js
+++ screens/profile.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import {StyleSheet, Image, View} from 'react-native';
+import { StyleSheet, Image, View, AsyncStorage } from 'react-native';
 import { Container, Header, Content, Footer, FooterTab, Button, Icon, Text, List, ListItem, Thumbnail, Body, Left, Right, Switch, Title } from 'native-base';
-
-import PhotoUpload from 'react-native-photo-upload'
+import Orientation from 'react-native-orientation';
+import PhotoUpload from 'react-native-photo-upload';
 //import footer style
 import footer_styles from './style/footer';
 import main_styles from './style/main';
@@ -10,190 +10,209 @@ import main_styles from './style/main';
 import lang from './localization/fa.json';
 
 
-export default class profile_page extends React.Component{
+export default class profile_page extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            user_profile:
+                {
+                    name: "",
+                    last_name: "",
+                    tel: ""
+                }
+        };
+        Orientation.lockToPortrait();
+        AsyncStorage.getItem('user_profile', (err, result) => {
+            if (result != null) {
+                //  alert(result);
+                var global_data = JSON.parse(result);
+                this.setState({ user_profile: global_data });
 
+            }
+        });
+    }
 
     async componentWillMount() {
         await Expo.Font.loadAsync({
-          'Roboto': require('native-base/Fonts/Roboto.ttf'),
-          'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+            'Roboto': require('native-base/Fonts/Roboto.ttf'),
+            'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
         });
-      }
+    }
 
     static navigationOptions = {
-        title:'',
+        title: '',
         header: null,
-    }; 
-    render(){ 
-        var {navigate}=this.props.navigation; 
-        return(
-            <Container> 
+    };
+    render() {
+        var { navigate } = this.props.navigation;
+        return (
+            <Container>
                 <Header style={footer_styles.header}>
                     <Left>
                         <Button transparent
-                        onPress={()=>this.props.navigation.replace("Home") }>
-                        <Icon style={footer_styles.header_btn} name='arrow-back' />
+                            onPress={() => this.props.navigation.replace("Home")}>
+                            <Icon style={footer_styles.header_btn} name='arrow-back' />
                         </Button>
                     </Left>
                     <Body>
                         <Title style={footer_styles.header_btn}>
-                        {lang.profile}
+                            {lang.profile}
                         </Title>
                     </Body>
                     <Right>
                         <Button transparent
-                        onPress={()=>this.props.navigation.replace("edit_profile") }
-                        > 
+                            onPress={() => this.props.navigation.replace("edit_profile")}
+                        >
                             <Text style={footer_styles.header_btn}>ویرایش</Text>
                         </Button>
                     </Right>
                 </Header>
-                <Content>                   
-                <View style={{flex: 1, flexDirection: 'column',justifyContent: 'center',alignItems: 'center',padding: 10 , backgroundColor:'#ffffff',paddingBottom:30}}>
-                    <View>
-                        <PhotoUpload
-                            onPhotoSelect={avatar => {
-                                if (avatar) {
-                                console.log('Image base64 string: ', avatar)
-                                }
-                            }}
-                            >
-                            <Image
-                                style={{
-                                paddingVertical: 30,
-                                width: 150,
-                                height: 150,
-                                borderRadius: 75
+                <Content>
+                    <View style={{ flex: 1, flexDirection: 'column', justifyContent: 'center', alignItems: 'center', padding: 10, backgroundColor: '#ffffff', paddingBottom: 30 }}>
+                        <View>
+                            <PhotoUpload
+                                onPhotoSelect={avatar => {
+                                    if (avatar) {
+                                        console.log('Image base64 string: ', avatar)
+                                    }
                                 }}
-                                resizeMode='cover'
-                                source={
-                                require('./img/default_avatar.jpg')
-                                }
-                            />
-                        </PhotoUpload>
+                            >
+                                <Image
+                                    style={{
+                                        paddingVertical: 30,
+                                        width: 150,
+                                        height: 150,
+                                        borderRadius: 75
+                                    }}
+                                    resizeMode='cover'
+                                    source={
+                                        require('./img/default_avatar.jpg')
+                                    }
+                                />
+                            </PhotoUpload>
+                        </View>
+                        <View>
+                            <Text>{this.state.user_profile.name + " " + this.state.user_profile.last_name}</Text>
+                        </View>
+                        <View>
+                            <Text>{this.state.user_profile.tel}</Text>
+                        </View>
                     </View>
-                    <View>
-                        <Text>نام و نام خانوادگی</Text>
-                    </View>
-                    <View>
-                        <Text>+۹۸ ۹۳۶ ۵۸۲ ۰۷۲۳</Text>
-                    </View>
-                </View>
-                 <List style={main_styles.list}>
-                    <ListItem itemDivider style={main_styles.list_div}>
-                        <Text></Text>
-                    </ListItem>     
-                    <ListItem icon>
-                        <Left>
-                            
-                            <Icon name="arrow-back" />
-                        </Left>
-                        <Body>
-                            <Text>زبان</Text>
-                        </Body>
-                        <Right>
-                        <Icon name="map" />
-                        </Right>
-                    </ListItem>
-                    <ListItem icon>
-                    <Left>
-                        
-                        <Icon name="arrow-back" />
-                    </Left>
-                    <Body>
-                        <Text>پیام ها</Text>
-                    </Body>
-                    <Right>
-                    <Icon name="mail" />
-                    </Right>
-                    </ListItem>
-                    <ListItem itemDivider style={main_styles.list_div}>
-                        <Text></Text>
-                    </ListItem>     
-                    <ListItem icon
-                    onPress={()=>this.props.navigation.replace("upload_file",{name:lang.national_card_imge,parent:"profile"})}
-                    >
-                        <Left>
-                        <Icon name="checkmark" /> 
-                        </Left>
-                        <Body>
-                        <Text>
-                            {lang.national_card_imge}
-                        </Text>                           
-                        </Body>
-                        <Right>
-                        <Icon name="card" />
-                        </Right>
-                    </ListItem>
-                    <ListItem icon
-                   onPress={()=>this.props.navigation.replace("upload_file",{name:lang.Photo_ID,parent:"profile"})}
-                    >
-                    <Left>
-                    <Icon name="checkmark" />
-                    </Left>
-                    <Body>
-                        <Text>
-                            {lang.Photo_ID}
-                        </Text>
-                    </Body>
-                    <Right>
-                    <Icon name="card" />
-                    </Right>
-                    </ListItem>
-                    <ListItem itemDivider style={main_styles.list_div}>
-                        <Text></Text>
-                    </ListItem>     
-                    <ListItem icon>
-                        <Left>
-                            <Icon name="arrow-back" />
-                        </Left>
-                        <Body>
-                        <Text>راهنما</Text>                           
-                        </Body>
-                        <Right>
-                        <Icon name="help-buoy" />
-                        </Right>
-                    </ListItem>
-                </List>
-            </Content>
-            <Footer 
-                style={ footer_styles.footer_body }
-            >
-                 <FooterTab
-                style={ footer_styles.footer_body }>
-                    <Button 
-                        vertical
-                        onPress={()=>this.props.navigation.replace("files") }
-                    >
-                        <Icon active name="folder-open" style={footer_styles.footer_btn} />
-                        <Text style={footer_styles.footer_btn}>
-                            {lang.file}
-                        </Text>
-                    </Button>
-                    <Button 
-                        vertical
-                        onPress={()=>this.props.navigation.replace("message") }
-                    >
-                        <Icon active name="ios-chatbubbles" style={footer_styles.footer_btn} />
-                        <Text style={footer_styles.footer_btn}>
-                            {lang.messages}
-                        </Text>
-                    </Button>
-                    <Button 
-                        vertical
-                        onPress={()=>this.props.navigation.replace("Home") }
-                    >
-                        <Icon active name="md-home" style={footer_styles.footer_btn} />
-                        <Text style={footer_styles.footer_btn}>
-                            {lang.home}
-                        </Text>
-                    </Button>
-                </FooterTab>
-            </Footer>
+                    <List style={main_styles.list}>
+                       {/* <ListItem itemDivider style={main_styles.list_div}>
+                            <Text></Text>
+                        </ListItem>
+                         <ListItem icon>
+                            <Left>
+
+                                <Icon name="arrow-back" />
+                            </Left>
+                            <Body>
+                                <Text>زبان</Text>
+                            </Body>
+                            <Right>
+                                <Icon name="map" />
+                            </Right>
+                        </ListItem>
+                        <ListItem icon>
+                            <Left>
+
+                                <Icon name="arrow-back" />
+                            </Left>
+                            <Body>
+                                <Text>پیام ها</Text>
+                            </Body>
+                            <Right>
+                                <Icon name="mail" />
+                            </Right>
+                        </ListItem> */}
+                        <ListItem itemDivider style={main_styles.list_div}>
+                            <Text></Text>
+                        </ListItem>
+                        <ListItem icon
+                            onPress={() => this.props.navigation.replace("upload_file", { name: lang.national_card_imge, parent: "profile" })}
+                        >
+                            <Left>
+                                <Icon name="checkmark" />
+                            </Left>
+                            <Body>
+                                <Text>
+                                    {lang.national_card_imge}
+                                </Text>
+                            </Body>
+                            <Right>
+                                <Icon name="card" />
+                            </Right>
+                        </ListItem>
+                        {/* <ListItem icon
+                            onPress={() => this.props.navigation.replace("upload_file", { name: lang.Photo_ID, parent: "profile" })}
+                        >
+                            <Left>
+                                <Icon name="checkmark" />
+                            </Left>
+                            <Body>
+                                <Text>
+                                    {lang.Photo_ID}
+                                </Text>
+                            </Body>
+                            <Right>
+                                <Icon name="card" />
+                            </Right>
+                        </ListItem> */}
+                        <ListItem itemDivider style={main_styles.list_div}>
+                            <Text></Text>
+                        </ListItem>
+                        <ListItem icon>
+                            <Left>
+                                <Icon name="arrow-back" />
+                            </Left>
+                            <Body>
+                                <Text>راهنما</Text>
+                            </Body>
+                            <Right>
+                                <Icon name="help-buoy" />
+                            </Right>
+                        </ListItem>
+                    </List>
+                </Content>
+                <Footer
+                    style={footer_styles.footer_body}
+                >
+                    <FooterTab
+                        style={footer_styles.footer_body}>
+                        <Button
+                            vertical
+                            onPress={() => this.props.navigation.replace("files")}
+                        >
+                            <Icon active name="folder-open" style={footer_styles.footer_btn} />
+                            <Text style={footer_styles.footer_btn}>
+                                {lang.file}
+                            </Text>
+                        </Button>
+                        <Button
+                            vertical
+                            onPress={() => this.props.navigation.replace("message")}
+                        >
+                            <Icon active name="ios-chatbubbles" style={footer_styles.footer_btn} />
+                            <Text style={footer_styles.footer_btn}>
+                                {lang.messages}
+                            </Text>
+                        </Button>
+                        <Button
+                            vertical
+                            onPress={() => this.props.navigation.replace("Home")}
+                        >
+                            <Icon active name="md-home" style={footer_styles.footer_btn} />
+                            <Text style={footer_styles.footer_btn}>
+                                {lang.home}
+                            </Text>
+                        </Button>
+                    </FooterTab>
+                </Footer>
             </Container>
         );
     }
 
-} 
+}
 
 
--- screens/profile_edit.js
+++ screens/profile_edit.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, ListView} from 'react-native';
+import {StyleSheet, ListView, AsyncStorage} from 'react-native';
 import { Container, Header, Title, Content, Footer, FooterTab, Button, Icon, Text, List, ListItem, Thumbnail, Body, Left, Right, Form, Item, Input, Label } from 'native-base';
 import Orientation from 'react-native-orientation';
 //import footer style
@@ -9,15 +9,34 @@ import lang from './localization/fa.json';
 
 import home_styles from './style/home';
 
+import server_url from './config/server_url.json';
+
+import axios from 'axios';
+
 
 export default class profile_edit_page extends React.Component{
     constructor(props) {
         super(props);
         this.state = {
+          user_profile:
+            {name:"",
+            last_name:"",
+            tel:""
+            }, 
             name:null, 
-            last_name:null,
+            last_name:null
         };
         Orientation.lockToPortrait();
+        AsyncStorage.getItem('user_profile', (err, result) => {
+            if(result!= null){
+              //  alert(result);
+                var global_data = JSON.parse(result);
+                this.setState({user_profile:global_data});
+                this.setState({name:global_data.name});
+                this.setState({last_name:global_data.last_name});
+                
+            }
+        });
       }
       
     async componentWillMount() {
@@ -31,6 +50,55 @@ export default class profile_edit_page extends React.Component{
         title:'',
         header: null,
     }; 
+
+    btn_save_onclick(){
+        axios.post(server_url.mobo_user, {
+            act: 'mobo_users_edit_profile_by_ID',
+            ID: this.state.user_profile.ID,
+            name:this.state.name, 
+            last_name: this.state.last_name
+                      })
+          .then(response=> {
+            
+            if(response.data.msg != undefined || response.data.msg != null){
+                axios.post(server_url.mobo_user, {
+                    act: 'mobo_users_get_by_ID',
+                    ID: this.state.user_profile.ID,
+                  })
+                  .then(response=> {
+                    if(response.data[0].ID != undefined || response.data[0].ID != null){
+                        //this.get_data();
+                        //alert(response.data[0].ID);
+                        this.setState({user_id:response.data[0].ID});
+                        try {
+                            AsyncStorage.setItem('user_profile', JSON.stringify(response.data[0])); 
+                          } catch (error) {
+                            alert("can not write data in device");
+                          }
+                        
+                    }else{
+                        alert(JSON.stringify(response.data[0]));
+                    }
+                  
+                })
+                .catch(function (error) {
+                    alert("data load error");
+                  });
+                //alert(response.data.msg);
+                alert(lang.success);
+                this.props.navigation.replace("profile");
+                
+            }
+            if(response.data.data!= undefined || response.data.data != null ){
+                
+                alert(lang.send_message);
+            }
+                      
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+    }
     render(){ 
         var {navigate}=this.props.navigation; 
         return(
@@ -58,6 +126,7 @@ export default class profile_edit_page extends React.Component{
                             <Input
                                 maxLength={50}
                                 onChange={(event) => this.setState({name: event.nativeEvent.text})}
+                                value={this.state.name}
                                 />
                                 
                         </Item>
@@ -68,12 +137,14 @@ export default class profile_edit_page extends React.Component{
                             <Input
                                 maxLength={50}
                                 onChange={(event) => this.setState({last_name: event.nativeEvent.text})}
+                                value={this.state.last_name}
                                 />
                                 
                         </Item>
                     </Form>
                     <Button
-                    style={{marginTop:20}}>   
+                    style={{marginTop:20}}
+                    onPress={()=>{this.btn_save_onclick()}}>   
                         <Body>
                             <Text style={{color:"#ffffff"}}>
                                 {lang.save}
